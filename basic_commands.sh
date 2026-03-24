8f0056fe09d02cdeffa15a8bfe7d65428f228789
JustinYSJSU <justinkaiy02@gmail.com>
2026-03-24 11:23:49 -0700

    Add command for parameter

--- basic_commands.sh
+++ basic_commands.sh
@@ -13,3 +13,15 @@ ros2 topic list
 
 # list messages from a certain topic
 ros2 topic echo <name>
+
+# list node parameters
+ros2 param list
+
+# list value of a specfic parameter
+ros2 param get /<node> <param_name>
+
+# view type + constraints of param
+ros2 param describe /<node> <param_name>
+
+# change / set param value
+ros2 param set /<node> <param_name>
